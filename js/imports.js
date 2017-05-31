6004c7278d6f956405302349251a9f1955b777a5
Javier Gamarra <nhpatt@gmail.com>
2017-05-31 12:21:33 +0200

    use imports

---
+++ js/imports.js
@@ -0,0 +1,4 @@
+HTMLImports.whenReady(function () {
+    menu.appendChild(document.getElementById('link-menu').import.body);
+    footer.appendChild(document.getElementById('link-footer').import.body);
+});
