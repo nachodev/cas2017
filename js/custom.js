55a632a8b5263f3b3e8fec4e72d11726dd41c79e
Javier Gamarra <nhpatt@gmail.com>
2017-06-02 18:06:17 +0200

    analytics

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+HTMLImports.whenReady(function () {
+    menu.appendChild(document.getElementById('link-menu').import.childNodes[0]);
+    footer.appendChild(document.getElementById('link-footer').import.childNodes[0]);
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-100345104-1', 'auto');
+ga('send', 'pageview');
--- js/imports.js
+++
@@ -1,4 +0,0 @@
-HTMLImports.whenReady(function () {
-    menu.appendChild(document.getElementById('link-menu').import.childNodes[0]);
-    footer.appendChild(document.getElementById('link-footer').import.childNodes[0]);
-});
