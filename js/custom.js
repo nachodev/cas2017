5c327be1d634a15e83abb894df09dcd8cda24377
Javier Gamarra <nhpatt@gmail.com>
2017-07-02 18:21:47 +0200

    translate landing

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,24 @@
 HTMLImports.whenReady(function () {
     menu.appendChild(document.getElementById('link-menu').import.childNodes[0]);
     footer.appendChild(document.getElementById('link-footer').import.childNodes[0]);
+
+    var i18n = window.domI18n({
+        selector: '[data-translatable]',
+        separator: ' // ',
+        languages: ['es', 'en'],
+        currentLanguage: 'es',
+        defaultLanguage: 'es'
+    });
+
+    var changeLanguage = document.getElementById('change-language');
+    var language = 'es';
+    changeLanguage.onclick = (e) => {
+        language = language === 'es' ? 'en' : 'es';
+        changeLanguage.textContent = language === 'es' ? 'English web' : 'Web en español';
+        i18n.changeLanguage(language);
+        window.location.hash = '#' + language;
+        e.preventDefault();
+    };
 });
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
