f9c3383747736d58bb06328673bb074f02bf2c34
Javier Gamarra <nhpatt@gmail.com>
2017-07-03 12:11:38 +0200

    add hash propagation

--- js/custom.js
+++ js/custom.js
@@ -24,12 +24,29 @@ HTMLImports.whenReady(function () {
         language = 'en';
         i18n.changeLanguage(language);
     }
+
+    var nodes = document.querySelectorAll('[hasheable]');
+
+    for (var i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        node.onclick = function () {
+            window.location.assign(node.href + '#' + language);
+            return false;
+        }
+    }
 });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-100345104-1', 'auto');
 ga('send', 'pageview');
